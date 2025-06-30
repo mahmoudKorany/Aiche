01484ed8cdd93e093699b1494523f79a6fde2e2e
mahmoudKorany <mahmoud01090677934@gmail.com>
2025-06-30 21:34:58 +0300

    final edit app

--- lib/welcome_screens/splash_screen/splash_screen.dart
+++ lib/welcome_screens/splash_screen/splash_screen.dart
@@ -127,7 +127,7 @@ class _SplashScreenState extends State<SplashScreen>
                                                 scale: _scaleAnimation,
                                                 child: Image.asset(
                                                   'assets/images/splash.png',
-                                                  height: 150.h,
+                                                  height: 200.h,
                                                 ),
                                               ),
                                               const Gap30(),
